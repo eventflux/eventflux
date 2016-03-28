39b37872f5394b7091a70d6fea6fcad386cb6f6f
Gerardduch <gerard.duch@est.fib.upc.edu>
2016-03-28 14:57:21 +0200

    crear evento, consultar listado eventos y consultar eventos hechos, no esta comprobado si funcionan correctamente

---
+++ server/routes/evento.js
@@ -0,0 +1,71 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var express_jwt = require('express-jwt');
+var config = require('../config');
+var ObjectId = require('mongoose').Types.ObjectId;
+
+
+var router = require('express').Router();
+
+
+var EventoModel = mongoose.model('EventoModel');
+var UsuarioModel = mongoose.model('UsuarioModel');
+
+
+
+ // Obtener lista eventos
+router.get('/listaEventos', function(req, res) {
+    EventModel.find({/*aqui no ponemos ninguna condicion ya que los queremos todos*/}, function(err, eventos) {
+        if (err) res.status(500).json(err);
+        else res.status(200).json(eventos); //retornamos la lista de los eventos en formato JSON
+    });
+});
+
+// Obtener usuario
+router.get('/consultarEvento/:titulo/:fecha', function(req, res) { //supondre que se identifica por titulo-fecha
+    EventModel.findOne({ titulo: req.params.titulo, fecha: req.params.fecha}, function(err, evento) {
+        if (err) res.status(500).json(err);
+        else res.status(200).json(evento);
+    });
+});
+
+// A partir de aquí todas las rutas van a requerir estar autenticado
+// Por eso usamos el middleware de express_jwt, pasandole el SECRET para
+// que pueda desencriptar el token y comprobar que es correcto
+// Como especificamos requestProperty: 'usuario' en req.usuario tendremos
+// la info del usuario desencriptada
+router.use(express_jwt({ secret: config.JWT_SECRET, requestProperty: 'usuario' }));
+
+// Crear evento
+router.post('/newEvent', function(req, res) {
+    var eventoInstance = new EventoModel(req.body);
+    UsuarioModel.findOne({email: req.user.email}, function(err, infoUser) {
+         if (err) res.status(500).send(err);
+         else {
+            eventoInstance.organizador = infoUser._id;
+            eventoInstance.save(function(err, newEvento) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    else res.status(200).json({ evento: newEvento });
+                }
+            });
+         }
+    });
+
+});
+
+// Si no ha entrado en ninguna ruta anterior, error 404 not found
+router.all('*', function(req, res) { res.status(404).send("Error 404 not found"); });
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
