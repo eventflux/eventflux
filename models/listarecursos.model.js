27a85605bd42bb9662a3fbef5839118390e9df78
Gerardduch <gerard.duch@est.fib.upc.edu>
2016-04-20 17:04:33 +0200

    anadir recursos arregalda, falta debugar un pel mes

--- models/listarecursos.model.js
+++ models/listarecursos.model.js
@@ -2,19 +2,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 module.exports = function() {
-  var recursosModelSchema = new Schema([
-	{
-  		nombre: {type: String, require: true},
+  var recursosModelSchema = new Schema({
+  		nombre: {type: String, required: true},
 	  	cantidad: Number,
 	  	recompensa: String,
 	  	solicitudes: [{
-	  		idUsuario: {type: Schema.Types.ObjectId, ref: 'EventoModel'},
+	  		idUsuario: {type: Schema.Types.ObjectId, ref: 'UsuarioModel'},
 	  		cantidad: Number,
 	  		aceptado: {Boolean, default: false}
 	  	}],
 	  	eventoID: {type: Schema.Types.ObjectId, ref: 'EventoModel'}
-  	}
-  ]);
+  	});
 
   recursosModelSchema.index({ nombre: 1, eventoID: 1}, { unique: true });
 
--- routes/evento.js
+++ routes/evento.js
@@ -13,6 +13,15 @@ var UsuarioModel = mongoose.model('UsuarioModel');
 var ListaRecursosModel = mongoose.model('ListaRecursosModel');
 
 
+ // Obtener lista eventos
+router.get('/listaRecursosComplet', function(req, res) {
+    //var now = new Date();
+    //console.log(now.getDay());
+    ListaRecursosModel.find({/*aqui no ponemos ninguna condicion ya que los queremos todos*/}, function(err, eventos) {
+        if (err) res.status(500).json(err);
+        else res.status(200).json(eventos); //retornamos la lista de los eventos en formato JSON 
+    });
+});
 
  // Obtener lista eventos
 router.get('/listaEventosComplet', function(req, res) {
@@ -25,8 +34,6 @@ router.get('/listaEventosComplet', function(req, res) {
 });
 
 router.get('/listaEventos', function(req, res) {
-    //var now = new Date();
-    //console.log(now.getDay());
     EventoModel.find({/*aqui no ponemos ninguna condicion ya que los queremos todos*/}, function(err, eventos) {
         if (err) res.status(500).json(err);
         else {
@@ -136,7 +143,87 @@ router.post('/newEvent', function(req, res) {
     });
 });
 
+router.post('/anadirRecursos/:ubicacion/:fechaIni', function(req, res) {
+    EventoModel.findOne({ ubicacion: req.params.ubicacion, fechaIni: req.params.fechaIni}, function(err, evento) {
+        if (err) {
+            res.status(500).json(err);
+        }
+        else if (!evento) {
+            res.status(404).json(err);
+        }
+        else {
+            console.log(req.body.recursos);
+            for(var i = 0; i<req.body.recursos.length; ++i) {
+                //do something with e.g. req.body[key]
+                var rec = req.body.recursos[i];
+                console.log("rec->", JSON.parse(JSON.stringify(rec)));
+                var listaRecursosInstance = new ListaRecursosModel(rec);
+                console.log("rec2->"+listaRecursosInstance);
+                console.log('flor', JSON.parse(JSON.stringify(listaRecursosInstance)))
+                listaRecursosInstance.save(function(err, newRecurso) {
+                    if (err) {
+                        res.status(500).send(err);
+                    } else {
+                        console.log("newrecurso->"+JSON.stringify(newRecurso));
+                        var query = { ubicacion: req.params.ubicacion, fechaIni: req.params.fechaIni};
+                        console.log(newRecurso._id);
+                        var update = { $push: { listaRecursos: newRecurso._id } };
 
+                        // Indica que queremos que el objeto que nos devuelva la callback (updated)
+                        // sea el nuevo (después de haberle aplicado la actualización) y no el viejo
+                        // Si no lo ponemos por defecto nos pone el viejo
+                        var options = { 'new': true };
+
+                        EventoModel.findOneAndUpdate(query, update, options, function(err, updated) {
+                            if (err) {
+                                res.status(500).json(err);
+                            }
+                            else {
+                                //res.status(200).json(updated);
+                            }
+                        });
+                    }
+                });
+            }
+            res.status(200).json("correcte");
+        }
+
+            /*for (var k = 0; k < req.body.length; ++k) {
+                var aux = req.body[k];
+                console.log("aux->"+JSON.stringify(aux));
+                var listaRecursosInstance = new ListaRecursosModel(req.body[k]);
+                listaRecursosInstance.save(function(err, newRecurso) {
+                    if (err) {
+                        res.status(500).send(err);
+                    } else {
+                        console.log("newrecurso->"+newRecurso);
+                        var query = { ubicacion: req.params.ubicacion, fechaIni: req.params.fechaIni};
+                        console.log(newRecurso._id);
+                        var update = { $push: { listaRecursos: newRecurso._id } };
+
+                        // Indica que queremos que el objeto que nos devuelva la callback (updated)
+                        // sea el nuevo (después de haberle aplicado la actualización) y no el viejo
+                        // Si no lo ponemos por defecto nos pone el viejo
+                        var options = { 'new': true };
+
+                        EventoModel.findOneAndUpdate(query, update, options, function(err, updated) {
+                            if (err) {
+                                res.status(500).json(err);
+                            }
+                            else {
+                                //res.status(200).json(updated);
+                            }
+                        });
+                    }
+                });
+            }
+            res.status(200).json("correcte");
+        }*/
+    });
+});
+
+
+/*
 router.post('/anadirRecursos/:ubicacion/:fechaIni', function(req, res) {
     var listaRecursosNueva = req.body;
     if(!listaRecursosNueva.length) res.status(510).json("Debes introducir una lista. Aunque solo haya un recurso por añadir");
@@ -150,13 +237,13 @@ router.post('/anadirRecursos/:ubicacion/:fechaIni', function(req, res) {
                         if (err) res.status(500).json(err);
                         else {
                             //recursos tiene la lista de los recursos del evento en cuestion
-                            if (listaRecursosActual == null) listaRecursosActual = [];
+                            //if (listaRecursosActual == null) listaRecursosActual = [];
                             var trobat;
                             for (var i = 0; i < listaRecursosNueva.length; i++) {                     
                                 trobat = false;
                                 for (var j = 0; j < listaRecursosActual.length && !trobat; j++) { 
                                     if (listaRecursosNueva[i].nombre == listaRecursosActual[j].nombre) {
-                                        console.log("entro trobat");
+                                        //console.log("entro trobat");
                                         //hay una coincidencia con un nombre, con lo que se modifica automaticamente cantidad y recompensa, para no eliminar las solicitudes
                                         listaRecursosActual[j].cantidad = listaRecursosNueva[i].cantidad;
                                         listaRecursosActual[j].recompensa = listaRecursosNueva[i].recompensa;
@@ -164,23 +251,30 @@ router.post('/anadirRecursos/:ubicacion/:fechaIni', function(req, res) {
                                     }
                                 }
                                 if (!trobat) {
-                                    console.log("entro no trobat");
-                                    console.log("Que posem?->"+JSON.stringify(listaRecursosNueva[i]));
+                                    //console.log("entro no trobat");
+                                    //console.log("Que posem?->"+JSON.stringify(listaRecursosNueva[i]));
                                     //no s'ha trobat, per lo que hem de crear el recurs a la bd
+                                    console.log(listaRecursosNueva);
+                                    console.log(listaRecursosNueva[0]);
+                                    var s = ""+listaRecursosNueva[i];
                                     listaRecursosActual.push(listaRecursosNueva[i]);
+
                                 }
                             }
                             //Ahora en listaRecursosActual hay la nueva lista, con lo que tenemos que guardar eso
 
-                            console.log("listaRecursosActual->"+JSON.stringify(listaRecursosActual)+"<-----");
-                            console.log("listaRecursosNueva->"+JSON.stringify(listaRecursosNueva)+"<-----");
+                            //console.log("listaRecursosActual->"+JSON.stringify(listaRecursosActual)+"<-----");
+                            //console.log("listaRecursosNueva->"+JSON.stringify(listaRecursosNueva)+"<-----");
 
                             //hasta ahi todo bien
                             for (var k = 0; k < listaRecursosActual.length; k++) {
+                                console.log("k->"+k+"<-----");
                                 //actualizamos todas los recursos
                                 if (!listaRecursosActual[k]._id) {
+                                    console.log("listRec de k->"+JSON.stringify(listaRecursosActual[k])+"<-");
                                     //creamos el recurso
                                     var recursoInstance = new ListaRecursosModel(listaRecursosActual[k]);
+                                    console.log("recinsta->"+JSON.stringify(recursoInstance));
                                     recursoInstance.save(function(err, newRecurso) {
                                         if (err) res.status(500).send(err);
                                         else {
@@ -222,15 +316,19 @@ router.post('/anadirRecursos/:ubicacion/:fechaIni', function(req, res) {
         });
     }
 });
+*/
 
 
 router.get('/listaRecursos/:ubicacion/:fechaIni', function(req, res) {
     EventoModel.findOne({ ubicacion: req.params.ubicacion, fechaIni: req.params.fechaIni }, function(err, evento) {
         if (err) res.status(500).json(err);
         else {
+            console.log("EVENTO: \n" + evento);
             ListaRecursosModel.find({ eventoID: evento._id }, function(err, recursos) {
                 if (err) res.status(500).json(err);
-                else res.status(200).json(recursos);
+                else {
+                    res.status(200).json(recursos);
+                }
             });
             
         }
@@ -291,20 +389,6 @@ router.delete('/borrarRecurso/:ubicacion/:fechaIni/:nombreRecurso', function(req
     });
 });
 
-/*
-userRouter.delete('/craftworld/:id_craftworld/borrarArma/:id_arma', express_jwt({secret: jwt_secret, requestProperty: 'admin'}), function(req, res, next) {
-  console.log(req.admin);
-  var cwId = req.params.id_craftworld;
-  var aId = req.params.id_arma;
-  if (req.admin.rango != "admin") res.status(200).json("No esta logeado con un usuario con poderes de administrador");
-  else {
-    Arma.remove({_id: new ObjectId(aId)}, function(err){
-    if(!err) {
-      res.status(200).end();
-    }
-  });
-  }
-});*/
 
 router.post('/confirmarParticipacion/:ubicacion/:fechaIni/:nombreRecurso/:usario', function(req, res) {
     EventoModel.findOne({ ubicacion: req.params.ubicacion, fechaIni: req.params.fechaIni }, function(err, eventos) {
@@ -313,10 +397,12 @@ router.post('/confirmarParticipacion/:ubicacion/:fechaIni/:nombreRecurso/:usario
             ListaRecursosModel.find({ eventoID: eventos._id }, function(err, recursos) {
                 if (err) res.status(500).json(err);
                 else { 
-                //     var trobat = false;
-                //     for (var i = 0; i < recursos.length && !trobat; ++i ) {
-                //         if(recursos[i].nombre == req.body.nombreRecurso ) {
-                //             trobat = true;
+                    var trobat = false;
+                    for (var i = 0; i < recursos.length && !trobat; ++i ) {
+                        if(recursos[i].nombre == req.body.nombreRecurso ) {
+                            trobat = true;
+                            console.log( recursos[i].solicitudes );
+                            
                 //             ListaRecursosModel.remove({ _id: new ObjectId(recursos[i]._id )}, function(err){
                 //                 if(!err) {
                 //                   res.status(200).end();
@@ -324,14 +410,14 @@ router.post('/confirmarParticipacion/:ubicacion/:fechaIni/:nombreRecurso/:usario
                 //             });
                 //             console.log("borrado");
                 //         }
-                //     }
+                        }
                 //     res.status(200).json(recursos); 
+                    }
                 }
             });
         }
     });
 });
-
 
 // Si no ha entrado en ninguna ruta anterior, error 404 not found
 router.all('*', function(req, res) { res.status(404).send("Error 404 not found"); });
